257327e768143bd4409ea78bbc8877b0c15cf7bc
Alexander Grinevich <accdev@ukr.net>
2023-06-28 21:27:03 -0400

    add editable flag to account records

--- force-app/main/default/lwc/financialServicesAccountsList/financialServicesAccountsList.js
+++ force-app/main/default/lwc/financialServicesAccountsList/financialServicesAccountsList.js
@@ -1,23 +1,25 @@
 import { LightningElement, wire } from 'lwc';
-//import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getFinancialAccounts from '@salesforce/apex/AccountController.getFinancialAccounts';
 
 const COLUMNS = [
     {
         label: 'Account Name', fieldName: 'recordUrl', type: 'url', sortable: true,
         typeAttributes: { label: { fieldName: 'name' } },
+        editable: true
     },
     {
-        label: 'Account Owner', fieldName: 'ownerName', sortable: true
+        label: 'Account Owner', fieldName: 'ownerName', sortable: true,
+        displayReadOnlyIcon: true
     },
     {
-        label: 'Phone', fieldName: 'phone', type: 'phone'
+        label: 'Phone', fieldName: 'phone', type: 'phone', editable: true
     },
     {
-        label: 'Website', fieldName: 'website', type: 'url'
+        label: 'Website', fieldName: 'website', type: 'url', editable: true
     },
     {
-        label: 'Annual Revenue', fieldName: 'annualRevenue', type: 'number'
+        label: 'Annual Revenue', fieldName: 'annualRevenue', type: 'number', editable: true
     }
 ];//Account Name, Account Owner, Phone, Website, and Annual Revenue
 
@@ -29,9 +31,15 @@ export default class FinancialServicesAccountsList extends LightningElement {
     searchByNameTerm = '';
     columns = COLUMNS;
     rows = [];
+    displayedRows = [];
 
     sortBy;
     sortDirection;
+
+    searchTerm = '';
+    debounceTimer;
+
+    draftValues;
 
 
     @wire(getFinancialAccounts, { searchByNameTerm: '$searchByNameTerm' })
@@ -39,21 +47,18 @@ export default class FinancialServicesAccountsList extends LightningElement {
         if (data) {
             //this.columns = data.columns;
             this.rows = data.rows;
+            this.displayedRows = JSON.parse(JSON.stringify(this.rows));
         } else if (error) {
             this.rows = [];
+            this.showError(error);
         }
     }
-
+    //sort methods
     handleSortData(event) {
-        console.log('handleSortData');
         if (this.rows && this.rows.length > 0) {
-            console.log('handleSortData.2');
             const fieldName = this.getSortingFieldName(event.detail.fieldName);
-            console.log('handleSortData.fieldName:', fieldName);
             this.sortBy = event.detail.fieldName;
-            console.log('handleSortData.sortBy:', this.sortBy);
             this.sortDirection = event.detail.sortDirection;
-
             this.sortData(fieldName, event.detail.sortDirection);
         }
     }
@@ -68,7 +73,6 @@ export default class FinancialServicesAccountsList extends LightningElement {
         const isReverse = sortDirection === 'asc' ? 1 : -1;
 
         const getValue = (data) => {
-            console.log('getValue.val:', data[fieldName]);
             return data[fieldName];
         }
 
@@ -79,6 +83,42 @@ export default class FinancialServicesAccountsList extends LightningElement {
             return isReverse * ((a > b) - (b > a));
         });
 
-        this.rows = rowsToSort;
+        this.displayedRows = rowsToSort;
+    }
+    // search methods
+    handleSearch(event) {
+        const { value } = event.target;
+
+        clearTimeout(this.debounceTimer);
+        this.debounceTimer = setTimeout(() => {
+            this.searchTerm = value.toUpperCase();
+            this.filterRecords();
+        }, 300);
+    }
+
+    filterRecords() {
+        if (this.searchTerm) {
+            //filter
+            const filteredRows = this.rows.filter(item => item.name?.toUpperCase().includes(this.searchTerm));
+            this.displayedRows = JSON.parse(JSON.stringify(filteredRows));
+        } else { // show all records
+            this.displayedRows = JSON.parse(JSON.stringify(this.rows));
+        }
+    }
+
+    //edit/save methods
+    handleSave(event) {
+
+    }
+
+    showError(error) {
+        const msg = error?.message || error?.body?.message || error;
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error',
+                message: msg,
+                variant: 'error'
+            })
+        );
     }
 }
